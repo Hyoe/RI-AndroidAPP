6a527c10855ba8fa4be9fe6dc22851c3a15fb970
JFransen44 <jfransen@csumb.edu>
2016-05-29 20:23:51 -0700

    adding comments. business detail activity now displays relevant info

--- app/src/main/java/com/example/jfransen44/recycleit/BusinessDetailActivity.java
+++ app/src/main/java/com/example/jfransen44/recycleit/BusinessDetailActivity.java
@@ -8,19 +8,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.jfransen44.recycleit.MultiSpinner.MultiSpinnerListener;
 
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.TreeMap;
-import com.example.jfransen44.recycleit.MultiSpinner;
-import com.example.jfransen44.recycleit.MultiSpinner.MultiSpinnerListener;
 public class BusinessDetailActivity extends AppCompatActivity {
 
     ImageView businessImage;
+    TextView businessName;
+    TextView businessAddress;
+    TextView businessPhone;
+    TextView businessURL;
+    TextView businessHours;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +33,12 @@ public class BusinessDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_business_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        businessName = (TextView) findViewById(R.id.businessName);
+        businessAddress = (TextView) findViewById(R.id.businessAddress);
+        businessPhone = (TextView) findViewById(R.id.businessPhone);
+        businessHours = (TextView) findViewById(R.id.businessHours);
+        businessURL = (TextView) findViewById(R.id.businessURL);
 
         businessImage = (ImageView) findViewById(R.id.businessImage);
         //businessImage.
@@ -53,10 +64,25 @@ public class BusinessDetailActivity extends AppCompatActivity {
             }
         });
 
-        /*String[] businessDetail = this.getIntent().getStringArrayExtra("businessDetails");
-        for (int i = 0; i < 5; i++){
+        /* Contents of businessDetail array
+        Index:
+        0: Business Name
+        1: Business Address
+        2: Business Phone Number
+        3: Business Photo URL
+        4: Business Website URL
+        5: Business Hours, separated by comma
+         */
+        String[] businessDetail = this.getIntent().getStringArrayExtra("businessDetails");
+        for (int i = 0; i < 6; i++){
             Log.d("DETAIL ACT", businessDetail[i]);
-        }*/
+        }
+        businessDetail[5].replace(',', '\n');
+        businessName.setText(businessDetail[0]);
+        businessAddress.setText(businessDetail[1]);
+        businessPhone.setText(businessDetail[2]);
+        businessHours.setText(businessDetail[5]);
+        businessURL.setText(businessDetail[4]);
     }
 
     public Drawable loadImageFromWeb (String url){
