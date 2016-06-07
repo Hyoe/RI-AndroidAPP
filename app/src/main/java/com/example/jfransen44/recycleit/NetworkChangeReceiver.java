f82ac801bc21559a9445f0cb8a1d8e1684002bd9
shawnmcbrayer <semcbrayer@gmail.com>
2016-06-07 15:59:26 -0700

    Fixed network connection listener bug

--- app/src/main/java/com/example/jfransen44/recycleit/NetworkChangeReceiver.java
+++ app/src/main/java/com/example/jfransen44/recycleit/NetworkChangeReceiver.java
@@ -13,25 +13,19 @@ public class NetworkChangeReceiver extends BroadcastReceiver {
         int status = NetworkUtil.getConnectivityStatusString(context);
         if (!"android.net.conn.CONNECTIVITY_CHANGE".equals(intent.getAction())) {
             if(status==NetworkUtil.NETWORK_STATUS_NOT_CONNECTED){
-                //new NoInternet(context).execute();
-                //intent = new Intent(NetworkChangeReceiver.this, NoInternet.class);
-                //startActivity(intent);
-                Log.d(">>>>>>>>>>>>>>", "network DOWN!");
-                //context.startActivity(new Intent(context, NoInternet.class));
 
                 Intent i = new Intent(context, NoInternet.class);
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(i);
             }else{
-                //new ResumeForceExitPause(context).execute();
-                Log.d(">>>>>>>>>>>>>>", "network UPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP!");
-                //context.startActivity(new Intent(context, MainActivity.class));
                 Intent i = new Intent(context, MainActivity.class);
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(i);
-
             }
-
+        }else{
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
         }
     }
 }
