aa9374e342ef42237b1fd1d4bead4ddfbc13329c
shawnmcbrayer <semcbrayer@gmail.com>
2016-06-08 17:21:20 -0700

    fixed network detection bug

--- app/src/main/java/com/example/jfransen44/recycleit/NetworkChangeReceiver.java
+++ app/src/main/java/com/example/jfransen44/recycleit/NetworkChangeReceiver.java
@@ -13,18 +13,18 @@ public class NetworkChangeReceiver extends BroadcastReceiver {
         int status = NetworkUtil.getConnectivityStatusString(context);
         if (!"android.net.conn.CONNECTIVITY_CHANGE".equals(intent.getAction())) {
             if(status==NetworkUtil.NETWORK_STATUS_NOT_CONNECTED){
-               // Log.d(">>>>>>>>", "Shutting Down: " + status);
+               //Log.d(">>>>>>>>>", "Shutting Down: " + status);
                 Intent i = new Intent(context, NoInternet.class);
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(i);
             }else{
-             //   Log.d(">>>>>>>>", "Starting up inner else");
+                //Log.d(">>>>>>>>>", "Starting up inner else");
                 Intent i = new Intent(context, MainActivity.class);
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(i);
             }
-        }else{
-           // Log.d(">>>>>>>>", "Starting up: " + status);
+        }else if (status!=NetworkUtil.NETWORK_STATUS_NOT_CONNECTED){
+            //Log.d(">>>>>>>>>", "Starting up: " + status);
 
             Intent i = new Intent(context, MainActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
